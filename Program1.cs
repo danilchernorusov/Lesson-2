0bc5d238b6cd5640fb616a80f1d096f64e6d1051
danilchernorusov <danilchernorusov@gmail.com>
2023-05-01 16:58:42 +0300

    commit

---
+++ Program1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int notebooks, pens;
+
+        double priceNotebook, prisePen, money;
+
+        do
+        {
+            Console.Write("Скiльки зошитiв треба купити: ");
+            notebooks = int.Parse(Console.ReadLine());
+        } while (notebooks < 0);
+        do
+        {
+            Console.Write("Скiльки ручок треба купити: ");
+            pens = int.Parse(Console.ReadLine());
+        } while(pens < 0);
+
+
+        do
+        {
+            Console.Write("Скiльки коштує один зошит: ");
+            priceNotebook = double.Parse(Console.ReadLine());
+        } while (priceNotebook < 0);
+        do
+        {
+            Console.Write("Скiльки коштує одна ручка: ");
+            prisePen = double.Parse(Console.ReadLine());
+        } while (prisePen < 0);
+
+
+        do
+        {
+            Console.Write("Скiльки у вас грошей: ");
+            money = double.Parse(Console.ReadLine());
+        } while (money < 0);
+
+
+
+        double priceNotebooks = notebooks * priceNotebook;
+        double prisePens = pens * prisePen;
+        double prise = priceNotebooks + prisePens;
+
+        if (money > prise)
+        {
+            double remained = money - prise;
+            Console.WriteLine($"Пiсля покупки у вас залишиться {remained} грн.");
+        }
+        else if (money == prise)
+        {
+            Console.WriteLine("Пiсля покупки у вас не залишиться жодної гривнi.");
+        }
+        else
+        {
+            Console.WriteLine("За наявної кiлькостi грошей ви не зможете здiйснити таку покупку.");
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int number;
+        string numberString, maxNumber = "";
+        do
+        {
+            Console.Write("Введiть додатне тризначне число: ");
+            number = Convert.ToInt32(Console.ReadLine());
+        } while (number < 100 | number >= 1000);
+
+        numberString = Convert.ToString(number);
+
+        for (int i = 0; i < 3; i++)
+        {
+            maxNumber = maxNumber + numberString.Max();
+            string numberString2 = "";
+            for (int n = 0; n < numberString.Length;  n++) 
+            {
+                if (numberString[n] != numberString.Max())
+                {
+                    numberString2 = numberString2 + numberString[n];
+                }
+            }
+            numberString = numberString2;
+
+        }
+
+        Console.WriteLine(maxNumber);
+    }
+}
